e3cc6628537a97decb64a52c3a86d1ecdb5e9b8b
Josef Bauer <josef.bauer.1st@gmail.com>
2024-03-05 18:26:10 +0100

    Add nvim kickstart config

---
+++ .config/nvim-kickstart/lua/custom/plugins/init.lua
@@ -0,0 +1,30 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'folke/noice.nvim',
+    lazy = false,
+    event = 'VeryLazy',
+    opts = {},
+    config = function()
+      require('noice').setup {}
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    config = function()
+      require('auto-session').setup()
+    end,
+  },
+}
