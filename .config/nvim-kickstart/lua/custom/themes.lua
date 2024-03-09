827a4f96061657e1bc6429fb43315cd52635971b
Josef Bauer <josef.bauer.1st@gmail.com>
2024-03-09 21:04:12 +0100

    Nvin: disable hover effect

--- .config/nvim-kickstart/lua/custom/themes.lua
+++ .config/nvim-kickstart/lua/custom/themes.lua
@@ -4,9 +4,6 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here
-      -- vim.cmd.colorscheme 'tokyonight-moon'
-
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
     end,
@@ -17,8 +14,6 @@ return {
     priority = 1000,
     config = function()
       vim.o.background = 'dark' -- or 'light'
-
-      -- vim.cmd.colorscheme 'solarized'
     end,
   },
   {
