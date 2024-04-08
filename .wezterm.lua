a462349b61f7c2288fe68db73af33d4feeed1cfc
Josef Bauer <josef.bauer.1st@gmail.com>
2024-04-08 22:07:55 +0200

    Wezterm: disable fancy tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,7 +13,7 @@ config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.audible_bell = "Disabled"
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 64
--- config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = false
 config.enable_scroll_bar = false
 config.window_padding = {
 	left = 0,
