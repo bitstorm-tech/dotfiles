86a4f1e3a401d18ceffdce0dbb83287b2de1b881
Josef Bauer <bauer.josef1@swm.de>
2024-04-11 12:44:47 +0200

    Wezterm: change font weight to medium

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,8 +9,8 @@ config.color_scheme = 'Solarized Dark (Gogh)'
 -- config.font = wezterm.font("JetbrainsMono Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("UbuntuMono Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("ZedMono Nerd Font Mono", { weight = "Regular", stretch = "Expanded" })
-config.font = wezterm.font("Zed Mono", { weight = "Regular" })
-config.font_size = 16
+config.font = wezterm.font("Zed Mono", { weight = "Medium" })
+config.font_size = 17
 
 config.default_cursor_style = 'SteadyUnderline'
 -- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
