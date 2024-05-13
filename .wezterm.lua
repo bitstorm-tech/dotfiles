c9a3a13a07eb493e081e53052e56ffe0e80c4055
Josef Bauer <josef.bauer.1st@gmail.com>
2024-05-13 22:38:12 +0200

    Wezterm: change margin and cursor

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,7 +14,7 @@ config.font = wezterm.font("JetbrainsMono Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("Monaco", { weight = "Regular" })
 config.font_size = 17
 
-config.default_cursor_style = 'SteadyBlock'
+config.default_cursor_style = 'SteadyBar'
 -- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.audible_bell = "Disabled"
 config.hide_tab_bar_if_only_one_tab = true
@@ -22,10 +22,10 @@ config.tab_max_width = 64
 config.use_fancy_tab_bar = false
 config.enable_scroll_bar = false
 config.window_padding = {
-	left = 0,
-	right = 0,
+	left = 5,
+	right = 5,
 	top = 0,
-	bottom = 0,
+	bottom = 10,
 }
 
 return config
