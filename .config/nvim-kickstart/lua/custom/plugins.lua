87e65cf1ad26213a49b60a4c24ac14434b5581dc
Josef Bauer <josef.bauer.1st@gmail.com>
2024-06-02 00:46:46 +0200

    Nvim: add autosave plugin

--- .config/nvim-kickstart/lua/custom/plugins.lua
+++ .config/nvim-kickstart/lua/custom/plugins.lua
@@ -36,4 +36,15 @@ return {
       },
     },
   },
+  {
+    'okuuva/auto-save.nvim',
+    cmd = 'ASToggle', -- optional for lazy loading on command
+    event = { 'InsertLeave' }, -- optional for lazy loading on trigger events
+    opts = {
+      debounce_delay = 250,
+      execution_message = {
+        enabled = false,
+      },
+    },
+  },
 }
