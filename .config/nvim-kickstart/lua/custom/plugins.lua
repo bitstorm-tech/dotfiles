dd2dfeb66bc3db358f0f77868b98ca77c07c56b8
Josef Bauer <josef.bauer.1st@gmail.com>
2024-08-04 22:37:12 +0200

    Nvim: increase delay for auto-save

--- .config/nvim-kickstart/lua/custom/plugins.lua
+++ .config/nvim-kickstart/lua/custom/plugins.lua
@@ -45,7 +45,7 @@ return {
     cmd = 'ASToggle', -- optional for lazy loading on command
     event = { 'InsertLeave' }, -- optional for lazy loading on trigger events
     opts = {
-      debounce_delay = 100,
+      debounce_delay = 300,
       execution_message = {
         enabled = false,
       },
