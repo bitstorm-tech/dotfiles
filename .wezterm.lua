ae6c0ddbc941bf9c26a36f05e26bf80b0f3afb82
Josef Bauer <josef.bauer.1st@gmail.com>
2024-11-24 17:02:55 +0100

    Wezterm: use Monaspice Nerd Font

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,13 +7,14 @@ config.color_scheme = "JetBrains Darcula"
 -- config.color_scheme = 'Tokyo Night Day'
 
 -- config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
-config.font = wezterm.font("JetbrainsMono Nerd Font", { weight = "Regular" })
+-- config.font = wezterm.font("JetbrainsMono Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("UbuntuMono Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("Zed Mono", { weight = "Regular" })
 -- config.font = wezterm.font("RobotoMono Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("DejaVuSansM Nerd Font Mono", { weight = "Regular" })
 -- config.font = wezterm.font("Monaco", { weight = "Regular" })
-config.font_size = 18
+config.font = wezterm.font("MonaspiceNe Nerd Font", { weight = "Regular" })
+config.font_size = 19
 
 config.default_cursor_style = "SteadyBlock"
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
