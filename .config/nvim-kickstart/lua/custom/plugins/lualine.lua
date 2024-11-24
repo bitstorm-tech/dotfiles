ec9b5fe30f0dfbb686c3efb732b351c8ff71dde2
Josef Bauer <josef.bauer.1st@gmail.com>
2024-11-24 17:08:31 +0100

    Nvim: move plugin configs to own file per plugin

---
+++ .config/nvim-kickstart/lua/custom/plugins/lualine.lua
@@ -0,0 +1,24 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  lazy = false,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          file_status = true, -- Displays file status (readonly status, modified status)
+          newfile_status = true, -- Display new file status (new file means no write after created)
+          path = 3,
+          -- 0: Just the filename
+          -- 1: Relative path
+          -- 2: Absolute path
+          -- 3: Absolute path, with tilde as the home directory
+          -- 4: Filename and parent dir, with tilde as the home directory
+
+          shorting_target = 40,
+        },
+      },
+    },
+  },
+}
