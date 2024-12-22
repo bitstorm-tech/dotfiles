f5458576fef1979cf6eda6a84c41bf180f2ed309
Josef Bauer <josef.bauer.1st@gmail.com>
2024-12-22 12:41:27 +0100

    Nvim: change lualine styling a bit

--- .config/nvim-kickstart/lua/custom/plugins/lualine.lua
+++ .config/nvim-kickstart/lua/custom/plugins/lualine.lua
@@ -3,13 +3,17 @@ return {
   lazy = false,
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
+    options = {
+      globalstatus = true,
+      section_separators = { left = '', right = '' },
+    },
     sections = {
       lualine_c = {
         {
           'filename',
           file_status = true, -- Displays file status (readonly status, modified status)
           newfile_status = true, -- Display new file status (new file means no write after created)
-          path = 3,
+          path = 1,
           -- 0: Just the filename
           -- 1: Relative path
           -- 2: Absolute path
