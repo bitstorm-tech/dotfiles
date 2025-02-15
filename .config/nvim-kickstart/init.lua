ee3c05d9119b8186fb0808a530f27b9cc02569c9
Josef Bauer <josef.bauer.1st@gmail.com>
2025-02-15 20:29:51 +0100

    Nvim: add phpactor

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -373,6 +373,7 @@ require('lazy').setup {
           filetypes = { 'html', 'ts', 'js' },
         },
         pylsp = {},
+        phpactor = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
