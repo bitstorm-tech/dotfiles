efc6a242f8d09015f686762d4d63ae1a9b36e876
Josef Bauer <bauer.josef1@swm.de>
2026-03-05 16:14:35 +0100

    Neovim: close all buffers with leader<ba>, also the last one

--- .config/neovim/lua/custom/commands.lua
+++ .config/neovim/lua/custom/commands.lua
@@ -1,13 +1,20 @@
-local function close_all_buffers_except_last_one()
+local function close_all_buffers()
   local buf_numbers = vim.api.nvim_list_bufs()
-  local current_buf = vim.api.nvim_get_current_buf()
 
   for _, buf_nr in ipairs(buf_numbers) do
     local is_nvim_tree = vim.bo[buf_nr].filetype == 'NvimTree'
-    if buf_nr ~= current_buf and not is_nvim_tree then
+    if not is_nvim_tree then
       vim.api.nvim_buf_delete(buf_nr, { force = true })
     end
   end
+
+  -- NvimTree fokussieren falls sichtbar, sonst leerer Buffer als Fallback
+  local view = require('nvim-tree.view')
+  if view.is_visible() then
+    require('nvim-tree.api').tree.focus()
+  else
+    vim.cmd('enew')
+  end
 end
 
-vim.api.nvim_create_user_command('CloseAllBuffers', close_all_buffers_except_last_one, { desc = 'Close all buffers except current and NvimTree' })
+vim.api.nvim_create_user_command('CloseAllBuffers', close_all_buffers, { desc = 'Close all buffers and focus NvimTree' })
